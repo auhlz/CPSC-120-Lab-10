63bdf68da84145ca15dd4694f1aadd3f697ff787
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-11-10 17:08:39 -0800

    formatted correct but lint and header do not work

--- part-2/counties_functions.cc
+++ part-2/counties_functions.cc
@@ -7,34 +7,25 @@
 // Lab 10-02
 // Partners: @dianasuceli
 //
-// Counties Functions 
+// Counties Functions
 //
 
 #include "counties_functions.h"
 
-  // TODO: Implement the function
-  // Write a loop which takes the name of each county from the vector
-  // ca_counties and adds it to all_counties_string along with a space.
-
 std::string AllCountiesString(
-    const std::vector<std::vector<std::string>>& ca_counties) {
+    const std::vector<std::vector<std::string>> &ca_counties) {
   std::string all_counties_string;
-for (int i = 0; i < ca_counties[0].size(); i ++) {
-  all_counties_string += ca_counties[0][i] + " ";
-}
+  for (int i = 0; i < ca_counties[0].size(); i++) {
+    all_counties_string += ca_counties[0][i] + " ";
+  }
 
   return all_counties_string;
 }
 
-  // TODO: Implement the function
-  // Write a loop which goes through the ca_counties vector looking
-  // for a match with the string match_county. When it finds a match,
-  // return the index of the match. If it is not found, return -1.
-
-int CountyIndex(const std::vector<std::vector<std::string>>& ca_counties,
-                const std::string& match_county) {
+int CountyIndex(const std::vector<std::vector<std::string>> &ca_counties,
+                const std::string &match_county) {
   int index{-1};
-  for (int i = 0; i < ca_counties[0].size(); i++){
+  for (int i = 0; i < ca_counties[0].size(); i++) {
     if (ca_counties[0][i] == match_county) {
       return i;
     }
