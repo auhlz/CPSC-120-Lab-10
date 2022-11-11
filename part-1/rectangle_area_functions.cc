8ce569eabf5f72b299738fa4495f13aeca818f18
Alex Labitigan <alexlabitigan@csu.fullerton.edu>
2022-11-10 17:06:38 -0800

    something wrong with lint

--- part-1/rectangle_area_functions.cc
+++ part-1/rectangle_area_functions.cc
@@ -7,12 +7,12 @@
 // Lab 10-01
 // Partners: @dianasuceli
 //
-// Rectangle functions
+// Rectangle area
 //
 
 int RectangleArea(int length, int width) {
   int area{0};
-  if  (length >= 0 && width > 0) {
+  if (length >= 0 && width > 0) {
     area = length * width;
   }
   return area;
